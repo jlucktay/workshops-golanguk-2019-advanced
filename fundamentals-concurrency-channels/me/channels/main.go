75d77403eaec93b86f6ae94798015a7c6b9e9cc6
James Lucktaylor <jlucktay+github@gmail.com>
2019-08-21 12:18:08 +0100

    Complete channels exercise.

--- fundamentals-concurrency-channels/me/channels/main.go
+++ fundamentals-concurrency-channels/me/channels/main.go
@@ -7,27 +7,36 @@ func process(messages chan string, quit chan struct{}) {
 	// until the channel is closed
 	for m := range messages {
 		// print the message with a for loop using range
+		fmt.Printf("message: %s\n", m)
 	}
 
 	// close the `quit` channel
+	close(quit)
 }
 
 func main() {
 	// declare the messages channel of type string and capacity of 5
+	messages := make(chan string, 5)
 
 	// declare a signal channel
+	quit := make(chan struct{})
 
 	// launch the `process` function in a goroutine
+	go process(messages, quit)
 
 	// declare 5 fruits in a []string
+	fruits := []string{"apple", "banana", "watermelon", "strawberry", "pear"}
 
 	// loop through the fruits and send them to the messages channel
 	for _, f := range fruits {
+		messages <- f
 	}
 
 	// close the messages channel
+	close(messages)
 
 	// wait for everything to finish (hint, block on the quit channel)
+	<-quit
 
 	fmt.Println("done")
 }
